bf8dd34514c2ba6b9608aac3d2ae010420869ce4
Nikita Shesterikov <nikitossh@mail.ru>
2018-08-01 11:05:14 +0300

    modified file names

--- src/main/java/com/shesterikov/babblo/model/CostCategory.java
+++ src/main/java/com/shesterikov/babblo/model/CostCategory.java
@@ -1,4 +1,13 @@
 package com.shesterikov.babblo.model;
 
-public class CostCategory extends Category{
+public class CostCategory extends AbstractCategory {
+    private String category;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
